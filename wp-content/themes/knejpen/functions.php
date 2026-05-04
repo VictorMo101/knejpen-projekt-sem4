3f38770720283815ed0a28f60e9a7cedaa7b2e36
VictorMo101 <vicmor01@easv365.dk>
2026-05-04 14:39:23 +0200

    fix: acf json path file

--- wp-content/themes/knejpen/functions.php
+++ wp-content/themes/knejpen/functions.php
@@ -1,12 +1,10 @@
 <?php
-// Save ACF field groups as JSON in /wp-content/acf-json/
 add_filter('acf/settings/save_json', function($path) {
-    return get_stylesheet_directory() . '/../../../acf-json';
+    return get_stylesheet_directory() . '/acf-json';
 });
 
-// Load ACF field groups from the same folder
 add_filter('acf/settings/load_json', function($paths) {
-    $paths[] = get_stylesheet_directory() . '/../../../acf-json';
+    $paths[] = get_stylesheet_directory() . '/acf-json';
     return $paths;
 });
 
