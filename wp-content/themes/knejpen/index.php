087c51098a7b63dd23b580db054d0bf2b129d573
MarkEASV <achson01@easv365.dk>
2026-05-06 04:49:34 +0200

    feat(style, layout): Added about and and event section

--- wp-content/themes/knejpen/index.php
+++ wp-content/themes/knejpen/index.php
@@ -1,9 +1,26 @@
 <?php get_header(); ?>
 
-<div class="container">
-    <h1>hello world</h1>
-    <button id="click-button">Click me</button>
+<section class="about">
+    <div class="about-container">
+        <div class="about-text">
+        <h2>Fags and hags</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unk sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.A 
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unk sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.A </p>
+        </div>
+    
+        <div class="about-image">
+            <img src="https://picsum.photos/500/500" alt="test ong">
+        </div>
+    </div>
+</section>
 
-</div>
+<section class="events">
+    <div class="events-container">
+        <span class="events-line"></span>
+        <h3>Events</h3>
+        <span class="events-line"></span>
+         
+    </div>
+</section>
 
 <?php get_footer(); ?>
