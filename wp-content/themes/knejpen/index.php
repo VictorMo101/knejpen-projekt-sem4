d9f922ca2a6159e8ed57e39498f198070478078e
VictorMo101 <vicmor01@easv365.dk>
2026-05-06 16:10:40 +0200

    feat: added about section from branch 'feature/about' (merge issue)

--- wp-content/themes/knejpen/index.php
+++ wp-content/themes/knejpen/index.php
@@ -10,8 +10,31 @@
             <img src="<?php echo esc_url('http://knejpen-projekt.dk/wp-content/uploads/2026/05/knejpen-logo-temp-1.png'); ?>" alt="Knejpen Logo">
         </div>
     </div>
-    <div class="delete">
+</section>
+
+<section class="about">
+    <div class="about-container">
+        <div class="about-text">
+        <h2>Bags and Bags</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unk sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.A
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unk sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.A </p>
+        </div>
+   
+        <div class="about-image">
+            <img src="https://picsum.photos/500/500" alt="test ong">
+        </div>
     </div>
 </section>
+
+
+<!-- <section class="events">
+    <div class="events-container">
+        <span class="events-line"></span>
+        <h3>Events</h3>
+        <span class="events-line"></span>
+         
+    </div>
+</section> -->
+
 
 <?php get_footer(); ?>
