d66d671a99864d1f99bac334b6daa77134d366fe
VictorMo101 <vicmor01@easv365.dk>
2026-05-07 19:41:08 +0200

    feat: header

--- wp-content/themes/knejpen/header.php
+++ wp-content/themes/knejpen/header.php
@@ -3,4 +3,13 @@
 <head>
     <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
+<body <?php body_class('has-fixed-header'); ?>>
+<header class="site-header" role="banner">
+    <a class="site-brand" href="<?php echo esc_url( home_url( '/#home' ) ); ?>">Knejpen</a>
+
+    <nav class="site-nav" aria-label="Primary">
+        <a href="<?php echo esc_url( home_url( '/#home' ) ); ?>">Home</a>
+        <a href="<?php echo esc_url( home_url( '/#about' ) ); ?>">About</a>
+        <a href="<?php echo esc_url( home_url( '/#events' ) ); ?>">Events</a>
+    </nav>
+</header>
