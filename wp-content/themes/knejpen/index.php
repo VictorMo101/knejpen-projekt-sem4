3cd6342e55f5fe1722ea5ad68dfb284253245151
VictorMo101 <vicmor01@easv365.dk>
2026-05-07 19:44:57 +0200

    fix: changed logo and background video

--- wp-content/themes/knejpen/index.php
+++ wp-content/themes/knejpen/index.php
@@ -3,11 +3,11 @@
 <section id="home">
     <div class="video-wrap">
         <video autoplay muted loop playsinline>
-            <source src="<?php echo esc_url('http://knejpen-projekt.dk/wp-content/uploads/2026/05/VID20260502170012-1.mp4'); ?>" alt="Knejpen Video" type="video/mp4">
+            <source src="<?php echo esc_url('http://knejpen-projekt.dk/wp-content/uploads/2026/05/knejpen-hero-video.mp4'); ?>" alt="Knejpen Video" type="video/mp4">
         </video>
 
         <div class="video-overlay">
-            <img src="<?php echo esc_url('http://knejpen-projekt.dk/wp-content/uploads/2026/05/knejpen-logo-temp-1.png'); ?>" alt="Knejpen Logo">
+            <img src="<?php echo esc_url('http://knejpen-projekt.dk/wp-content/uploads/2026/05/knejpen-hero-billede.png'); ?>" alt="Knejpen Logo">
         </div>
     </div>
 </section>
