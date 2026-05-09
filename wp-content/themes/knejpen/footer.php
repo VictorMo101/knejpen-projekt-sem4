92234788304abb7a6f9a3784b300fc8b39e9388b
VictorMo101 <vicmor01@easv365.dk>
2026-05-09 02:03:16 +0200

    feat: footer frontend

--- wp-content/themes/knejpen/footer.php
+++ wp-content/themes/knejpen/footer.php
@@ -1,7 +1,43 @@
 <?php wp_footer(); ?>
 
-<footer>
-    
+<footer class="site-footer" id="contact">
+	<div class="footer-inner">
+
+		<div class="footer-visit">
+			<h3>Besøg os</h3>
+			<p>
+                Knejpen
+                <br>
+                Strandby Plads 4
+                <br>
+                6700 Esbjerg
+			</p>
+		</div>
+
+		<div class="footer-social">
+			<h3>Følg os</h3>
+			<div class="social-grid">
+				<a href="#">Instagram</a>
+				<a href="#">Facebook</a>
+			</div>
+		</div>
+
+        <div class="footer-hours">
+            <h3>Åbningstider</h3>
+            <div class="footer-time"><span>man:</span><span>13:00-20:00</span></div>
+            <div class="footer-time"><span>tir - tor:</span><span>13:00-00:00</span></div>
+            <div class="footer-time"><span>fre - lør:</span><span>13:00-02:00</span></div>
+        </div>
+
+        <div class="footer-brand">
+			<img class="footer-logo" src="http://knejpen-projekt.dk/wp-content/uploads/2026/05/knejpen-hero-billede.png" alt="Knejpen logo">
+		</div>
+
+	</div>
+
+	<div class="footer-bottom">
+		<span>© 2026 Knejpen. All rights reserved.</span>
+	</div>
 </footer>
 </body>
 </html>
