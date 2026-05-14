c83b3cad6870422406d6b0ec1d9979de96c5601f
VictorMo101 <vicmor01@easv365.dk>
2026-05-14 18:06:33 +0200

    fix: style changes

--- wp-content/themes/knejpen/index.php
+++ wp-content/themes/knejpen/index.php
@@ -57,12 +57,12 @@
                             >
                         <?php endif; ?>
                         <div class="event-img-overlay">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={10} stroke="#FEF2A6" className="size-30" alt="Location Pin Icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#FEF2A6" class="size-30" aria-label="Location Pin Icon">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
                             </svg>
                             <p class="event-place"><?php the_field('event-place'); ?></p>
-                            <svg class="event-external" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#FEF2A6" className="size-6">
+                            <svg class="event-external" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#FEF2A6" class="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
                             </svg>
 
